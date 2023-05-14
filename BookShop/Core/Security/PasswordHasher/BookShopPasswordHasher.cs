0c5a9f63e4aa19712283f617128bc296fcb87d01
Amirhaf76 <44908316+amirhaf76@users.noreply.github.com>
2023-05-14 16:00:33 +0330

    Hashing password is debugged and fixed.

--- BookShop/Core/Security/PasswordHasher/BookShopPasswordHasher.cs
+++ BookShop/Core/Security/PasswordHasher/BookShopPasswordHasher.cs
@@ -1,29 +1,96 @@
 ﻿using Infrastructure.AutoFac.FlagInterface;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography;
 
 namespace BookShop.Core.Security.PasswordHasher
 {
     public class BookShopPasswordHasher<TUser> : IScope, IPasswordHasher<TUser> where TUser : class
     {
-        private readonly IHashingPasswordStrategy _hashPasswordStrategy;
+        private const KeyDerivationPrf PBKDF2_ALGO = KeyDerivationPrf.HMACSHA1;
+        private const int PBKDF2_ITERATION_COUNT = 1000;
+        private const int PBKDF2_SUBKEY_LENGTH = 5;
+        private const int SALT_SIZE = 4;
 
-        public BookShopPasswordHasher(IHashingPasswordStrategy hashPasswordStrategy)
+        public BookShopPasswordHasher()
         {
-            _hashPasswordStrategy = hashPasswordStrategy;
+
         }
 
         public string HashPassword(TUser user, string password)
         {
-            var hashedPassword = _hashPasswordStrategy.HashPassword(password);
+            var hashedPassword = HashPassword(password);
 
             return Convert.ToBase64String(hashedPassword);
         }
 
         public PasswordVerificationResult VerifyHashedPassword(TUser user, string hashedPassword, string providedPassword)
         {
-            var hashedprovidedPassword = _hashPasswordStrategy.HashPassword(providedPassword);
+            return VerifyHashedPassword(hashedPassword, providedPassword);
+        }
 
-            if (Convert.ToBase64String(hashedprovidedPassword).Equals(hashedPassword))
+        private static byte[] HashPassword(
+            string password,
+            int saltSize,
+            KeyDerivationPrf pbkdf2Algo,
+            int pbkdf2IterationCount,
+            int pbkdf2SubkeyLength)
+        {
+            var salt = RandomNumberGenerator.GetBytes(saltSize);
+
+            return HashPassword(password, salt, pbkdf2Algo, pbkdf2IterationCount, pbkdf2SubkeyLength);
+        }
+
+        private static byte[] HashPassword(
+            string password,
+            byte[] salt,
+            KeyDerivationPrf pbkdf2Algo,
+            int pbkdf2IterationCount,
+            int pbkdf2SubkeyLength)
+        {
+            var saltSize = salt.Length;
+
+            var subKey = KeyDerivation.Pbkdf2(password, salt, pbkdf2Algo, pbkdf2IterationCount, pbkdf2SubkeyLength);
+
+            var outputBytes = new byte[saltSize + pbkdf2SubkeyLength];
+
+            Buffer.BlockCopy(salt, 0, outputBytes, 0, saltSize);
+            Buffer.BlockCopy(subKey, 0, outputBytes, saltSize, pbkdf2SubkeyLength);
+
+            return outputBytes;
+        }
+
+        private static byte[] HashPassword(string password)
+        {
+            return HashPassword(
+                password,
+                SALT_SIZE,
+                PBKDF2_ALGO,
+                PBKDF2_ITERATION_COUNT,
+                PBKDF2_SUBKEY_LENGTH);
+        }
+
+        private static byte[] HashPassword(string password, byte[] salt)
+        {
+            return HashPassword(
+                password,
+                salt,
+                PBKDF2_ALGO,
+                PBKDF2_ITERATION_COUNT,
+                PBKDF2_SUBKEY_LENGTH);
+        }
+
+        private static PasswordVerificationResult VerifyHashedPassword(
+            string hashedPasswordBlocks,
+            string providedPassword)
+        {
+            var passwordData = Convert.FromBase64String(hashedPasswordBlocks);
+
+            var salt = passwordData.AsSpan(0, SALT_SIZE).ToArray();
+
+            var providedHashedPassword = HashPassword(providedPassword, salt);
+
+            if (Convert.ToBase64String(providedHashedPassword).Equals(hashedPasswordBlocks))
             {
                 return PasswordVerificationResult.Success;
             }
@@ -33,5 +100,6 @@ namespace BookShop.Core.Security.PasswordHasher
             }
         }
     }
+
 
 }
